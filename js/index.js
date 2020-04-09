4d083d8f37dea2008fddbcf9f6acf33f016da894
Afeez-Ola <afeezbolajiola@gmail.com>
2020-04-09 16:50:48 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,32 +1,81 @@
-function initMap() {
-    var locations = [
-        ['Ojota school', 6.581998, 3.376204, 1],
-        ['Ikeja city mall', 6.614519, 3.355585, 2],
-        ['city hall', 6.446667, 3.4, 1]
-    ];
+window.onload = () => {
+    displayStores();
 
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: new google.maps.LatLng(6.581998, 3.376204),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+
+var map;
+var markers = [];
+var infoWindow;
+
+function initMap() {
+    var sydney = {
+        lat: -33.863276,
+        lng: 151.107977
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: sydney,
+        zoom: 11,
+        mapTypeId: 'roadmap'
     });
 
-    var infowindow = new google.maps.InfoWindow();
+    showStoresMarkers();
+}
 
-    var marker, i;
 
-    for (i = 0; i < locations.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            map: map,
-            icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
-        });
+function displayStores() {
 
-        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-                infowindow.setContent(locations[i][0]);
-                infowindow.open(map, marker);
-            };
-        })(marker, i));
-    };
-};
+    var storesHtml = ''
+    for (var [index, store] of stores.entries()) {
+        var address = store.addressLines;
+        var phone = store.phoneNumber;
+
+        storesHtml += `
+        <div class="store-container">
+        <div class="store-info-container">
+            <div class="store-address">
+                <span>${address[0]}</span>
+                <span>${address[1]}</span>
+            </div>
+            <div class="store-phone-number">
+               ${phone}
+            </div>
+        </div>
+
+        <div class="store-number-container">
+            <div class="store-number">
+               ${index+1}
+            </div>
+        </div>
+    </div>
+        
+        `
+        document.querySelector('.stores-list').innerHTML = storesHtml;
+    }
+}
+
+function showStoresMarkers() {
+
+    for (var [index, store] of stores.entries()) {
+        var latlng = new google.maps.LatLng(
+            store["coordinates"].latitude,
+            store["coordinates"].longitude);
+
+        var name = store.name;
+        var address = store.addressLines[0];
+        createMarker(latlng, name, address, index);
+    }
+}
+
+function createMarker(latlng, name, address, index) {
+    var html = "<b>" + name + "</b> <br/>" + address;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: latlng
+    });
+    // google.maps.event.addListener(marker, 'click', function(e) {
+    //     infoWindow.setContent(html);
+    //     infoWindow.open(map, marker);
+    // });
+    markers.push(marker);
+
+}
